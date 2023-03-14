53e2bc22d403827ec76732f74b98c4e1f132b354
jieun <wldmsdlsns1@gmail.com>
2023-03-14 18:06:57 +0900

    피드백 리스트 (Ant Design 사용), 피드백 Write 대략 레이아웃 구성

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -1,5 +1,64 @@
-const Feedback = () => {
-    return <div>Feedback</div>;
-};
+import React from "react";
+import { Table } from "antd";
+import type { ColumnsType } from "antd/es/table";
 
-export default Feedback;
+interface DataType {
+    key: React.Key;
+    writingNumber: string;
+    name: string;
+    contents: string;
+    date: string;
+}
+
+const columns: ColumnsType<DataType> = [
+    {
+        title: "글번호",
+        width: 70,
+        dataIndex: "writingNumber",
+        key: "writingNumber",
+        fixed: "left",
+    },
+    {
+        title: "작성자",
+        width: 100,
+        dataIndex: "name",
+        key: "name",
+        fixed: "left",
+    },
+    {
+        title: "글내용",
+        dataIndex: "contents",
+        key: "1",
+        width: 150,
+    },
+
+    {
+        title: "작성날짜",
+        key: "operation",
+        fixed: "right",
+        width: 100,
+        render: () => <a>2023-03-14</a>,
+    },
+];
+
+const data: DataType[] = [];
+for (let i = 0; i < 100; i++) {
+    data.push({
+        key: i,
+        writingNumber: `${i}`,
+        name: `선생님`,
+        contents: `승현이 메롱`,
+        date: `London Park no. ${i}`,
+    });
+}
+
+const FeedBack: React.FC = () => (
+    <Table
+        columns={columns}
+        dataSource={data}
+        scroll={{ x: 1500, y: 600 }}
+        style={{ marginTop: "50px" }}
+    />
+);
+
+export default FeedBack;
--- src/pages/teacher/FeedbackWrite.tsx
+++ src/pages/teacher/FeedbackWrite.tsx
@@ -1,5 +1,59 @@
+import styled from "styled-components";
+
 const FeedbackWrite = () => {
-    return <div>FeedbackWrite</div>;
+    return (
+        <FeedbackWriteContainer>
+            <FeedbackWriteBox>
+                <FeedbackWriteHeader>
+                    <div>선생님</div>
+                    <div>글</div>
+                    <div>날짜</div>
+                </FeedbackWriteHeader>
+                <FeedbackWriteBody>
+                    <textarea
+                        cols={20}
+                        rows={50}
+                        style={{ width: "100%", height: "50%" }}
+                    >
+                        1234
+                    </textarea>
+                </FeedbackWriteBody>
+            </FeedbackWriteBox>
+        </FeedbackWriteContainer>
+    );
 };
+
+const FeedbackWriteContainer = styled.div`
+    display: block;
+    width: 1600px;
+    height: 85vh;
+    background-color: red;
+`;
+const FeedbackWriteBox = styled.div`
+    position: relative;
+    display: block;
+    width: 80%;
+    height: 90%;
+    background-color: green;
+    margin: 100px auto;
+    padding: 50px;
+`;
+const FeedbackWriteHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    height: 10%;
+    background-color: skyblue;
+    margin: 0 auto;
+    margin-top: 50px;
+    border-bottom: 2px solid #000;
+`;
+const FeedbackWriteBody = styled.div`
+    width: 80%;
+    height: 50%;
+    margin: 0 auto;
+    margin-top: 150px;
+    border-bottom: 2px solid #000;
+`;
 
 export default FeedbackWrite;
