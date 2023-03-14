85c4d2207a2f1b3568877f0499ae9f45ef32b413
tmdgus95 <8571053@naver.com>
2023-03-14 20:44:03 +0900

    로그인,아이디·패스워드 UI
    UI만 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,31 @@
+import { Outlet } from "react-router-dom";
+import { LoginContainer } from "../styles/Styles";
+
 const Login = () => {
-    return <div>Login</div>;
+    return (
+        <>
+            <LoginContainer>
+                <h2>학습관리시스템</h2>
+                <form>
+                    <input type="text" placeholder="USER ID" />
+
+                    <br />
+
+                    <input type="password" placeholder="PASSWORD" />
+
+                    <br />
+
+                    <button>LOG IN</button>
+                </form>
+
+                <div>
+                    <button>아이디 찾기</button>
+                    <button>비밀번호 찾기</button>
+                </div>
+            </LoginContainer>
+            <Outlet />
+        </>
+    );
 };
 
 export default Login;
