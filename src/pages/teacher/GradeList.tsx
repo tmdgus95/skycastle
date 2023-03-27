960e3196c8595b267ec16f7ff60c347829f92e18
Haeparic <v567v202@gmail.com>
2023-03-27 17:33:58 +0900

    성적 등록 수정중

--- src/pages/teacher/GradeList.tsx
+++ src/pages/teacher/GradeList.tsx
@@ -1,175 +1,64 @@
-import React, { useState } from "react";
-import { Col, DatePicker, Row, Space, Table, Tabs } from "antd";
-import type { ColumnsType } from "antd/es/table";
 import GradeRegist from "./GradeRegist";
-import TabPane from "antd/es/tabs/TabPane";
-import { GradeRegistListContainer } from "../../styles/TeacherStyles";
-import Search from "antd/es/input/Search";
-import moment from "moment";
-import dayjs from "dayjs";
 import TabMenu from "../../components/TabMenu";
 
-interface DataType {
-    key: React.Key;
-    name: string;
-    listening: number;
-    reading: number;
-    grammar: number;
-    vocabulary: number;
-    class: string;
-}
-
-const columns: ColumnsType<DataType> = [
-    {
-        title: "Name",
-        dataIndex: "name",
-    },
-    {
-        title: "분야",
-        children: [
-            {
-                title: "듣기",
-                dataIndex: "listening",
-                sorter: (a, b) => a.listening - b.listening,
-            },
-            {
-                title: "독해",
-                dataIndex: "reading",
-                sorter: (a, b) => a.reading - b.reading,
-            },
-            {
-                title: "문법",
-                dataIndex: "grammar",
-                sorter: (a, b) => a.grammar - b.grammar,
-            },
-            {
-                title: "어휘",
-                dataIndex: "vocabulary",
-                sorter: (a, b) => a.vocabulary - b.vocabulary,
-            },
-        ],
-    },
-    {
-        title: "Class",
-        dataIndex: "class",
-        key: "class",
-        filters: [
-            {
-                text: "A반",
-                value: "A반",
-            },
-            {
-                text: "B반",
-                value: "B반",
-            },
-            {
-                text: "C반",
-                value: "C반",
-            },
-            {
-                text: "D반",
-                value: "D반",
-            },
-        ],
-        onFilter: (value, record) =>
-            record.class.indexOf(value as string) === 0,
-    },
-];
-
-const data: DataType[] = [];
-for (let i = 0; i < 62; i++) {
-    data.push({
-        key: i,
-        name: `엄준식${i}`,
-        listening: 25,
-        reading: 25,
-        grammar: 25,
-        vocabulary: 25,
-        class: "A반",
-    });
-}
-
-// const rowSelection = {
-//   onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-//     console.log(
-//       `selectedRowKeys: ${selectedRowKeys}`,
-//       "selectedRows: ",
-//       selectedRows
-//     );
-//   },
-//   getCheckboxProps: (record: DataType) => ({
-//     disabled: record.name === "Disabled User", // Column configuration not to be checked
-//     name: record.name,
-//   }),
-// };
-
 const GradeList = () => {
-    const [write, setWrite] = useState(false);
-    const onSearch = (value: string) => console.log(value);
-    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  // const [write, setWrite] = useState(false);
+  // const onSearch = (value: string) => console.log(value);
+  // const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
 
-    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-        console.log("selectedRowKeys changed: ", newSelectedRowKeys);
-        setSelectedRowKeys(newSelectedRowKeys);
-    };
+  // const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+  //   console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+  //   setSelectedRowKeys(newSelectedRowKeys);
+  // };
 
-    const rowSelection = {
-        selectedRowKeys,
-        onChange: onSelectChange,
-    };
+  // const rowSelection = {
+  //   selectedRowKeys,
+  //   onChange: onSelectChange,
+  // };
 
-    const handleWriteChange = () => {
-        setWrite(!write);
-    };
+  // const handleWriteChange = () => {
+  //   setWrite(!write);
+  // };
 
-    const defaultMonth = moment(new Date()).format("YYYY/MM");
-    return (
+  // const defaultMonth = moment(new Date()).format("YYYY/MM");
+  return (
+    <>
+      <TabMenu menu={"성적 입력"} />
+      {/* {write ? ( */}
+      <GradeRegist />
+      {/* ) : (
         <>
-            <TabMenu menu={"성적 조회"} />
-            {write ? (
-                <GradeRegist
-                    write={write}
-                    handleWriteChange={handleWriteChange}
-                />
-            ) : (
-                <>
-                    <GradeRegistListContainer>
-                        <DatePicker
-                            defaultValue={dayjs(defaultMonth, "YYYY/MM")}
-                            picker="month"
-                            format={"YYYY년 MM월"}
-                            bordered={false}
-                            allowClear={false}
-                        />
-                        <Table
-                            className="t-grade"
-                            rowSelection={rowSelection}
-                            columns={columns}
-                            dataSource={data}
-                            footer={() => (
-                                <div className="flex justify-between">
-                                    <Search
-                                        placeholder="이름을 입력하세요"
-                                        onSearch={onSearch}
-                                    />
-                                    <button
-                                        className="grade-bt"
-                                        onClick={handleWriteChange}
-                                    >
-                                        등록
-                                    </button>
-                                </div>
-                            )}
-                            pagination={{
-                                position: ["bottomCenter"],
-                                showSizeChanger: false,
-                            }}
-                        />
-                    </GradeRegistListContainer>
-                </>
-            )}
+          <GradeRegistListContainer>
+            <DatePicker
+              defaultValue={dayjs(defaultMonth, "YYYY/MM")}
+              picker="month"
+              format={"YYYY년 MM월"}
+              bordered={false}
+              allowClear={false}
+            />
+            <Table
+              className="t-grade"
+              // rowSelection={rowSelection}
+              columns={columns}
+              dataSource={data}
+              footer={() => (
+                <div className="flex justify-between">
+                  <Search placeholder="이름을 입력하세요" onSearch={onSearch} />
+                  <button className="grade-bt" onClick={handleWriteChange}>
+                    등록
+                  </button>
+                </div>
+              )}
+              pagination={{
+                position: ["bottomCenter"],
+                showSizeChanger: false,
+              }}
+            />
+          </GradeRegistListContainer>
         </>
-    );
+      )} */}
+    </>
+  );
 };
 
 export default GradeList;
--- src/pages/teacher/GradeRegist.tsx
+++ src/pages/teacher/GradeRegist.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table, Form, Input, Button, DatePicker } from "antd";
+import { Table, Form, Input, Button, DatePicker, DatePickerProps } from "antd";
 import type { ColumnsType } from "antd/es/table";
 import { GradeRegistContainer } from "../../styles/TeacherStyles";
 import Search from "antd/es/input/Search";
@@ -8,14 +8,14 @@ import moment from "moment";
 import axios from "axios";
 
 interface DataType {
-    // key: React.Key;
-    key: number;
-    name: string;
-    listening: any;
-    reading: any;
-    grammar: any;
-    vocabulary: any;
-    // class: string;
+  key: React.Key;
+  seq: number;
+  name: string;
+  listening: number | null;
+  reading: number | null;
+  grammar: number | null;
+  vocabulary: number | null;
+  // class: string;
 }
 
 // type StudentType = {
@@ -24,260 +24,271 @@ interface DataType {
 // };
 
 const columns: ColumnsType<DataType> = [
-    {
-        title: "Name",
-        dataIndex: "name",
-        key: "name",
+  {
+    title: "Seq",
+    dataIndex: "seq",
+    key: "seq",
+    sorter: (a, b) => a.seq - b.seq,
+    render: (text, record) => (
+      <Form.Item name={["user", record.key, "seq"]} initialValue={text}>
+        {text}
+      </Form.Item>
+    ),
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "분야",
+    children: [
+      {
+        title: "듣기",
+        dataIndex: "listening",
+        key: "listening",
         render: (text, record) => (
-            <Form.Item name={["user", record.key, "name"]} initialValue={text}>
-                {text}
-            </Form.Item>
+          <Form.Item
+            name={["user", record.key, "listening"]}
+            initialValue={text}
+            rules={[
+              {
+                required: true,
+                message: "점수를 입력해주세요.",
+              },
+            ]}
+          >
+            <Input
+              size="small"
+              style={{
+                width: 80,
+                fontSize: 16,
+                textAlign: "center",
+              }}
+            />
+          </Form.Item>
         ),
-    },
-    {
-        title: "Key",
-        dataIndex: "key",
-        key: "key",
-    },
-    {
-        title: "분야",
-        children: [
-            {
-                title: "듣기",
-                dataIndex: "listening",
-                key: "listening",
-                render: (text, record) => (
-                    <Form.Item
-                        name={["user", record.key, "listening"]}
-                        initialValue={text}
-                    >
-                        <Input
-                            size="small"
-                            style={{
-                                width: 80,
-                                fontSize: 16,
-                                textAlign: "center",
-                            }}
-                        />
-                    </Form.Item>
-                ),
-            },
-            {
-                title: "독해",
-                dataIndex: "reading",
-                key: "reading",
-                render: (text, record) => (
-                    <Form.Item
-                        name={["user", record.key, "reading"]}
-                        initialValue={text}
-                    >
-                        <Input
-                            size="small"
-                            style={{
-                                width: 80,
-                                fontSize: 16,
-                                textAlign: "center",
-                            }}
-                        />
-                    </Form.Item>
-                ),
-            },
-            {
-                title: "문법",
-                dataIndex: "grammar",
-                key: "grammar",
-                render: (text, record) => (
-                    <Form.Item
-                        name={["user", record.key, "grammar"]}
-                        initialValue={text}
-                    >
-                        <Input
-                            size="small"
-                            style={{
-                                width: 80,
-                                fontSize: 16,
-                                textAlign: "center",
-                            }}
-                        />
-                    </Form.Item>
-                ),
-            },
-            {
-                title: "어휘",
-                dataIndex: "vocabulary",
-                key: "vocabulary",
-                render: (text, record) => (
-                    <Form.Item
-                        name={["user", record.key, "vocabulary"]}
-                        initialValue={text}
-                    >
-                        <Input
-                            size="small"
-                            style={{
-                                width: 80,
-                                fontSize: 16,
-                                textAlign: "center",
-                            }}
-                        />
-                    </Form.Item>
-                ),
-            },
-        ],
-    },
-    // {
-    //     title: "Class",
-    //     dataIndex: "class",
-    //     key: "class",
-    //     filters: [
-    //         {
-    //             text: "A반",
-    //             value: "A반",
-    //         },
-    //         {
-    //             text: "B반",
-    //             value: "B반",
-    //         },
-    //         {
-    //             text: "C반",
-    //             value: "C반",
-    //         },
-    //         {
-    //             text: "D반",
-    //             value: "D반",
-    //         },
-    //     ],
-    //     onFilter: (value, record) =>
-    //         record.class.indexOf(value as string) === 0,
-    // },
+      },
+      {
+        title: "독해",
+        dataIndex: "reading",
+        key: "reading",
+        render: (text, record) => (
+          <Form.Item
+            name={["user", record.key, "reading"]}
+            initialValue={text}
+            rules={[
+              {
+                required: true,
+                message: "점수를 입력해주세요.",
+              },
+            ]}
+          >
+            <Input
+              size="small"
+              style={{
+                width: 80,
+                fontSize: 16,
+                textAlign: "center",
+              }}
+            />
+          </Form.Item>
+        ),
+      },
+      {
+        title: "문법",
+        dataIndex: "grammar",
+        key: "grammar",
+        render: (text, record) => (
+          <Form.Item
+            name={["user", record.key, "grammar"]}
+            initialValue={text}
+            rules={[
+              {
+                required: true,
+                message: "점수를 입력해주세요.",
+              },
+            ]}
+          >
+            <Input
+              size="small"
+              style={{
+                width: 80,
+                fontSize: 16,
+                textAlign: "center",
+              }}
+            />
+          </Form.Item>
+        ),
+      },
+      {
+        title: "어휘",
+        dataIndex: "vocabulary",
+        key: "vocabulary",
+        render: (text, record) => (
+          <Form.Item
+            name={["user", record.key, "vocabulary"]}
+            initialValue={text}
+            rules={[
+              {
+                required: true,
+                message: "점수를 입력해주세요.",
+              },
+            ]}
+          >
+            <Input
+              size="small"
+              style={{
+                width: 80,
+                fontSize: 16,
+                textAlign: "center",
+              }}
+            />
+          </Form.Item>
+        ),
+      },
+    ],
+  },
+  // {
+  //     title: "Class",
+  //     dataIndex: "class",
+  //     key: "class",
+  //     filters: [
+  //         {
+  //             text: "A반",
+  //             value: "A반",
+  //         },
+  //         {
+  //             text: "B반",
+  //             value: "B반",
+  //         },
+  //         {
+  //             text: "C반",
+  //             value: "C반",
+  //         },
+  //         {
+  //             text: "D반",
+  //             value: "D반",
+  //         },
+  //     ],
+  //     onFilter: (value, record) =>
+  //         record.class.indexOf(value as string) === 0,
+  // },
 ];
 
-// const data: DataType[] = [];
-// for (let i = 0; i < 62; i++) {
-//     data.push({
-//         key: i,
-//         name: `엄준식${i}`,
-//         listening: 25,
-//         reading: 25,
-//         grammar: 25,
-//         vocabulary: 25,
-//         class: "A반",
-//     });
-// }
-
-// const rowSelection = {
-//   onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-//     console.log(
-//       `selectedRowKeys: ${selectedRowKeys}`,
-//       "selectedRows: ",
-//       selectedRows
-//     );
-//   },
-//   getCheckboxProps: (record: DataType) => ({
-//     disabled: record.name === "Disabled User", // Column configuration not to be checked
-//     name: record.name,
-//   }),
-// };
-
 interface Props {
-    write: boolean;
-    handleWriteChange: () => void;
+  write: boolean;
+  handleWriteChange: () => void;
 }
 
-const GradeRegist = ({ write, handleWriteChange }: Props) => {
-    // ant 테이블 관련
-    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-    const onSearch = (value: string) => console.log(value);
-    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-        console.log("selectedRowKeys changed: ", newSelectedRowKeys);
-        setSelectedRowKeys(newSelectedRowKeys);
+const GradeRegist = () => {
+  // ant 테이블 관련
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const onSearch = (value: string) => console.log(value);
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+  //   const rowSelection = {
+  //     selectedRowKeys,
+  //     onChange: onSelectChange,
+  //   };
+  const defaultMonth = moment(new Date()).format("YYYY/MM");
+
+  // 학생 리스트
+  const [studentList, setStudentList] = useState([]);
+  const getPosts = async () => {
+    const accessToken = window.localStorage.getItem("token");
+    await axios
+      .get("http://192.168.0.140:8686/api/class/student", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setStudentList(res.data.content);
+      });
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  console.log(studentList);
+
+  const list: DataType[] = studentList.map(
+    (item: { seq: number; name: string }, index) => {
+      return {
+        key: index,
+        seq: item.seq,
+        name: item.name,
+        listening: null,
+        reading: null,
+        grammar: null,
+        vocabulary: null,
+      };
+    }
+  );
+
+  console.log(list);
+
+  // 날짜
+  const [onDate, setOnDate] = useState<string>(
+    moment(new Date()).format("YYYYMM")
+  );
+  const onChange: DatePickerProps["onChange"] = (date, dateString) => {
+    console.log(date);
+    setOnDate(moment(dateString).format("YYYYMM"));
+    console.log(dateString);
+  };
+  console.log(onDate);
+  // console.log(moment(onDate).format("YYYYMM"));
+
+  // 제출
+  const onFinish = (values: any) => {
+    // console.log("Success:", values);
+    const body = {
+      yearmonth: onDate,
+      addGradeVO: values.user,
     };
-    const rowSelection = {
-        selectedRowKeys,
-        onChange: onSelectChange,
-    };
-    const defaultMonth = moment(new Date()).format("YYYY/MM");
+    console.log(body);
+  };
 
-    // 학생 리스트
-    const [studentList, setStudentList] = useState([]);
-    const getPosts = async () => {
-        const accessToken = window.localStorage.getItem("token");
-        await axios
-            .get("http://192.168.0.140:8686/api/class/student", {
-                headers: { Authorization: `Bearer ${accessToken}` },
-            })
-            .then((res) => {
-                console.log(res.data);
-                setStudentList(res.data.content);
-            });
-    };
-    useEffect(() => {
-        getPosts();
-    }, []);
-
-    console.log(studentList);
-
-    const list: DataType[] = studentList.map(
-        (item: { seq: number; name: string }) => {
-            return {
-                key: item.seq,
-                name: item.name,
-                listening: "",
-                reading: "",
-                grammar: "",
-                vocabulary: "",
-            };
-        }
-    );
-
-    console.log(list);
-
-    // 제출
-    const onFinish = (values: any) => {
-        console.log("Success:", values);
-    };
-    return (
-        <GradeRegistContainer>
-            <DatePicker
-                defaultValue={dayjs(defaultMonth, "YYYY/MM")}
-                picker="month"
-                format={"YYYY년 MM월"}
-                bordered={false}
-                allowClear={false}
-            />
-            <Form onFinish={onFinish}>
-                <Table
-                    className="t-grade"
-                    rowSelection={rowSelection}
-                    columns={columns}
-                    // dataSource={data}
-                    dataSource={list}
-                    footer={() => (
-                        <div className="flex justify-between">
-                            <Search
-                                placeholder="이름을 입력하세요"
-                                onSearch={onSearch}
-                            />
-                            <div className="flex gap-2">
-                                <button
-                                    className="grade-bt"
-                                    onClick={handleWriteChange}
-                                >
-                                    취소
-                                </button>
-                                <button className="grade-bt" type="submit">
-                                    등록
-                                </button>
-                            </div>
-                        </div>
-                    )}
-                    pagination={{
-                        position: ["bottomCenter"],
-                        showSizeChanger: false,
-                    }}
-                />
-            </Form>
-        </GradeRegistContainer>
-    );
+  return (
+    <GradeRegistContainer>
+      <DatePicker
+        defaultValue={dayjs(defaultMonth, "YYYY/MM")}
+        picker="month"
+        format={"YYYY-MM"}
+        bordered={false}
+        allowClear={false}
+        onChange={onChange}
+      />
+      <Form onFinish={onFinish}>
+        <Table
+          className="t-grade"
+          // rowSelection={rowSelection}
+          columns={columns}
+          dataSource={list}
+          footer={() => (
+            <div className="flex justify-between">
+              <Search placeholder="이름을 입력하세요" onSearch={onSearch} />
+              <div className="flex gap-2">
+                {/* <button className="grade-bt" onClick={handleWriteChange}>
+                  취소
+                </button> */}
+                <button className="grade-bt" type="submit">
+                  등록
+                </button>
+              </div>
+            </div>
+          )}
+          pagination={{
+            position: ["bottomCenter"],
+            showSizeChanger: false,
+          }}
+        />
+      </Form>
+    </GradeRegistContainer>
+  );
 };
 
 export default GradeRegist;
