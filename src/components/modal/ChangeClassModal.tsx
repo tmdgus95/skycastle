ca796b0ae16631e4dd6fe0c47229363d15fa6095
tmdgus95 <8571053@naver.com>
2023-03-28 17:15:42 +0900

    리액트 쿼리 유즈뮤테이션 적용

--- src/components/modal/ChangeClassModal.tsx
+++ src/components/modal/ChangeClassModal.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useMutation } from "react-query"; // useMutation import 추가
 import { HeaderInstance } from "../../api/axios";
 import Button from "../UI/Button";
 
@@ -10,25 +11,39 @@ type Props = {
     };
     setModal: (a: boolean) => void;
 };
+
 export default function ChangeClassModal({
     userInfo: { seq, className, name },
     setModal,
 }: Props) {
     const [Selected, setSelected] = useState(1);
-    console.log(Selected);
+
+    // useMutation으로 변경
+    const { mutate, isLoading } = useMutation(
+        () =>
+            HeaderInstance.post(`/api/class/${seq}/${Selected}`, {}).then(
+                (res) => res.data
+            ),
+        {
+            onSuccess: () => {
+                setModal(false);
+                alert("반 변경이 완료 되었습니다.");
+            },
+            onError: (error) => {
+                console.log(error);
+            },
+        }
+    );
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const body = {};
-        HeaderInstance.post(`/api/class/${seq}/${Selected}`, body)
-            .then((res) => console.log(res))
-            .then(() => setModal(false))
-            .then(() => alert("반 병경이 완료 되었습니다."))
-            .catch((err) => console.log(err));
+        mutate();
     };
+
     const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setSelected(parseInt(e.target.value));
     };
+
     return (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl overflow-hidden  w-[600px] h-[300px]">
             <h3 className="text-3xl pl-8 py-2  bg-menuColor">반 변경</h3>
