66bffd393c8da65ccdbda72b0d9b485ccd7468da
Park Sieun <pmj8856@naver.com>
2023-04-04 18:40:31 +0900

    student 월별 점수 변화 chart 디자인 수정

--- src/pages/student/MonthlyChangeScore.tsx
+++ src/pages/student/MonthlyChangeScore.tsx
@@ -101,13 +101,15 @@ const MonthlyChangeScore = () => {
 
     return (
         <>
-            <p>&#8226; <b>월별</b> 내 점수 변화</p>
+            <p>
+                &#8226; <b>월별</b> 내 점수 변화
+            </p>
             <ResponsiveBar
                 data={scoreLists2}
                 keys={["grammar", "vocabulary", "comprehension", "listening"]}
                 indexBy="testName"
-                margin={{ top: 100, right: 40, bottom: 50, left: 60 }}
-                padding={0.5}
+                margin={{ top: 100, right: 130, bottom: 50, left: 60 }}
+                padding={0.4}
                 maxValue={400}
                 valueScale={{ type: "linear" }}
                 indexScale={{ type: "band", round: true }}
@@ -119,18 +121,18 @@ const MonthlyChangeScore = () => {
                 axisTop={null}
                 axisRight={null}
                 axisBottom={{
-                    tickSize: 0,
-                    tickPadding: 10,
+                    tickSize: 5,
+                    tickPadding: 5,
                     tickRotation: 0,
-                    legend: "",
+                    legend: "월별 모의평가",
                     legendPosition: "middle",
                     legendOffset: 32,
                 }}
                 axisLeft={{
-                    tickSize: 0,
-                    tickPadding: 10,
+                    tickSize: 5,
+                    tickPadding: 5,
                     tickRotation: 0,
-                    legend: "",
+                    legend: "점수",
                     legendPosition: "middle",
                     legendOffset: -40,
                 }}
@@ -140,7 +142,31 @@ const MonthlyChangeScore = () => {
                     from: "color",
                     modifiers: [["darker", 1.6]],
                 }}
-                legends={[]}
+                // legends={[]}
+                legends={[
+                    {
+                        dataFrom: "keys",
+                        anchor: "bottom-right",
+                        direction: "column",
+                        justify: false,
+                        translateX: 120,
+                        translateY: 0,
+                        itemsSpacing: 2,
+                        itemWidth: 100,
+                        itemHeight: 20,
+                        itemDirection: "left-to-right",
+                        itemOpacity: 0.85,
+                        symbolSize: 20,
+                        effects: [
+                            {
+                                on: "hover",
+                                style: {
+                                    itemOpacity: 1,
+                                },
+                            },
+                        ],
+                    },
+                ]}
                 role="application"
                 ariaLabel="Nivo bar chart demo"
                 barAriaLabel={function (e) {
