e3929f3bc89b642dab500906e82a8ac1ee0f0158
tmdgus95 <8571053@naver.com>
2023-04-04 12:37:24 +0900

    헤더로고 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer } from "../styles/Styles";
-import { BsFillPersonFill } from "react-icons/bs";
+import { RiVipCrown2Fill } from "react-icons/ri";
+import { IoMdSchool } from "react-icons/io";
+import { FaPen } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
@@ -22,14 +24,19 @@ const Header = () => {
             navigate("/teacher/mypage");
         }
     };
-
+    const ICON =
+        role === "STUDENT" ? (
+            <IoMdSchool />
+        ) : role === "TEACHER" ? (
+            <FaPen />
+        ) : (
+            <RiVipCrown2Fill />
+        );
     return (
         <HeaderContainer>
             <div>
                 <span>환영합니다</span>
-                <span>
-                    <BsFillPersonFill />
-                </span>
+                <span>{ICON}</span>
                 {name ? (
                     <span>
                         {name}님{" "}
